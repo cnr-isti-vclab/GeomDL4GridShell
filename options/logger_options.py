74d2d2e27c28b5a8d4ae9c897ef48e144215467f
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-01-27 17:24:45 +0100

    Add files via upload

--- options/logger_options.py
+++ options/logger_options.py
@@ -5,6 +5,7 @@ class WandbLoggerOptions:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Options for WandbLogger.')
         self.parser.add_argument('--device', dest='device', type=str, default='cpu', help='Device for pytorch')
+        self.parser.add_argument('--project', dest='project', required=True, type=str, default='cpu', help='Wandb project name')
         self.parser.add_argument('--niter', dest='n_iter', type=int, default=100, help='Number of optimization steps')
         
     def parse(self):
