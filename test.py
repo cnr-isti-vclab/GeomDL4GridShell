8635151395be2ebef52456614be2f1e102b1f150
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-03-02 16:34:59 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from models.layers.featured_mesh import FeaturedMesh
-import torch
+from models.layers.dgcnn_layer import DGCNNLayer
 
 if __name__ == '__main__':
     fm = FeaturedMesh(file='/home/andrea/Scrivania/GridShellLearn-main/meshes/quinta2.ply')
     fm.compute_mesh_input_features()
+    in_channels = fm.input_features.shape[1]
+    out_channels = 1024
+    k = 100
+    dgcnn = DGCNNLayer(in_channels, out_channels, k)
+    print(dgcnn(fm.input_features))
