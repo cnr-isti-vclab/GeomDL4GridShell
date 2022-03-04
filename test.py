72966a0b5e4210b040964423f45ceea79da242e3
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-03-04 10:50:46 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from models.layers.featured_mesh import FeaturedMesh
-from models.layers.dgcnn_layer import DGCNNLayer
+from models.networks import DGCNNDisplacerNet
 
 if __name__ == '__main__':
-    fm = FeaturedMesh(file='/home/andrea/Scrivania/GridShellLearn-main/meshes/quinta2.ply')
+    device = 'cpu'
+    fm = FeaturedMesh(file='/home/andrea/Scrivania/GridShellLearn-main/meshes/F_waveArch_hi.ply', device=device)
     fm.compute_mesh_input_features()
     in_channels = fm.input_features.shape[1]
-    out_channels = 1024
-    k = 100
-    dgcnn = DGCNNLayer(in_channels, out_channels, k)
-    print(dgcnn(fm.input_features))
+    k = 16
+    dgcnn = DGCNNDisplacerNet(in_channels, k, aggr='mean').to(device)
+    out = dgcnn(fm.input_features)
+    print(out)
