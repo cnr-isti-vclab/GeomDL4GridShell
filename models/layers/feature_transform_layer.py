4231e2e7f6d6ab0385d1aa567bb163a5aded5258
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-06-14 19:28:19 +0200

    Add files via upload

--- models/layers/feature_transform_layer.py
+++ models/layers/feature_transform_layer.py
@@ -4,20 +4,21 @@ from torch_geometric.nn import Linear
 
 class FeatureTransformLayer(torch.nn.Module):
 
-    def __init__(self, no_blocks, out_channels):
+    def __init__(self, mask, out_channels):
         super(FeatureTransformLayer, self).__init__()
-        self.no_blocks = no_blocks
+        self.mask = mask
+        self.no_blocks = len(mask)
 
         # Initializing homogenization MLPs.
         for block in range(self.no_blocks):
             setattr(self, 'mlp_' + str(block), Sequential(Linear(-1, out_channels), Tanh()))
 
-    def forward(self, x, mask):
+    def forward(self, x):
         out_list = []
 
         for block in range(self.no_blocks):
             current_mlp = getattr(self, 'mlp_' + str(block))
-            out_list.append(current_mlp(x[:, mask[block]]))
+            out_list.append(current_mlp(x[:, self.mask[block]]))
         
         return torch.cat(out_list, dim=1)
 
