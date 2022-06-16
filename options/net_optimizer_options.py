8c952f24379970199eb033e7067e9fd5b4b3878a
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-06-16 11:36:38 +0200

    Add files via upload

--- options/net_optimizer_options.py
+++ options/net_optimizer_options.py
@@ -3,7 +3,7 @@ import argparse
 class NetOptimizerOptions:
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description='Options for LacconianNetOptimizer.')
+        self.parser = argparse.ArgumentParser(description='Options for StructuralNetOptimizer.')
         self.parser.add_argument('--device', dest='device', type=str, default='cpu', help='Device for pytorch')
         self.parser.add_argument('--meshpath', dest='path', type=str, required=True, help='Path to starting mesh')
         self.parser.add_argument('--niter', dest='n_iter', type=int, default=100, help='Number of optimization steps')
@@ -17,8 +17,6 @@ class NetOptimizerOptions:
         self.parser.add_argument('--beamload', dest='beam_have_load', action='store_true', default=False, help='If beams give load to verts or not')
         self.parser.add_argument('--itertimes', dest='take_times', action='store_true', default=False, help='If iteration and backward times are required or not')
         self.parser.add_argument('--knn', dest='no_knn', type=int, default=16, help='Number of nearest neighbors in dgcnn layers')
-        self.parser.add_argument('--transforminputfeatures', dest='transform_in_features', action='store_true', default='False')
-        self.parser.add_argument('--getloss', dest='get_loss', action='store_true', default='False')
 
     def parse(self):
         return self.parser.parse_args()
