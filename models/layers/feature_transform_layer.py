c3d8dcc55c3cdc4bd70d2a3254ac4c6504235093
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-06-22 10:27:53 +0200

    Add files via upload

--- models/layers/feature_transform_layer.py
+++ models/layers/feature_transform_layer.py
@@ -1,17 +1,22 @@
 import torch
-from torch.nn import Sequential, Tanh
+from torch.nn import Sequential, Tanh, Sigmoid
 from torch_geometric.nn import Linear
 
 class FeatureTransformLayer(torch.nn.Module):
 
-    def __init__(self, mask, out_channels):
+    def __init__(self, mask, out_channels, activation='tanh'):
         super(FeatureTransformLayer, self).__init__()
         self.mask = mask
         self.no_blocks = len(mask)
 
         # Initializing MLPs.
         for block in range(self.no_blocks):
-            setattr(self, 'mlp_' + str(block), Sequential(Linear(-1, out_channels), Tanh()))
+            if activation == 'tanh':
+                setattr(self, 'mlp_' + str(block), Sequential(Linear(-1, out_channels), Tanh()))
+            elif activation == 'sigmoid':
+                setattr(self, 'mlp_' + str(block), Sequential(Linear(-1, out_channels), Sigmoid()))
+            else:
+                raise ValueError('FeatureTransformLayer: inserted activation is not valid.')
 
     def forward(self, x):
         out_list = []
