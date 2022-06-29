0bf0694e736818ef09750f282628bc93bd0bc384
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-06-29 17:22:49 +0200

    Add files via upload

--- models/layers/gatv2_layer.py
+++ models/layers/gatv2_layer.py
@@ -10,6 +10,10 @@ class GATv2Layer(torch.nn.Module):
         self.k = k
         self.gatv2 = GATv2Conv(in_channels=-1, out_channels=out_channels, heads=heads, concat=False)
 
-    def forward(self, x, batch=None):
+    def forward(self, x, return_attention_weights=False, batch=None):
         edge_index = knn_graph(x, self.k, batch=batch, loop=False)
-        return self.gatv2(x, edge_index)
+        
+        if return_attention_weights:
+            return self.gatv2(x, edge_index, return_attention_weights=True)
+        else:
+            return self.gatv2(x, edge_index)
