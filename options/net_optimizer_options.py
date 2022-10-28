64bdb59b2b486320e686616bc69cfb416a859f90
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2022-10-28 16:44:54 +0200

    Add files via upload

--- options/net_optimizer_options.py
+++ options/net_optimizer_options.py
@@ -14,7 +14,6 @@ class NetOptimizerOptions:
         self.parser.add_argument('--displayinterval', dest='display_interval', type=int, default=1, help='Loss display iterations interval')
         self.parser.add_argument('--savelabel', dest='save_label', type=str, default='deformation', help='Label for ouputs')
         self.parser.add_argument('--losstype', dest='loss_type', type=str, default='norm_vertex_deformations', help='Type of loss computed')
-        self.parser.add_argument('--beamload', dest='beam_have_load', action='store_true', default=False, help='If beams give load to verts or not')
         self.parser.add_argument('--itertimes', dest='take_times', action='store_true', default=False, help='If iteration and backward times are required or not')
         self.parser.add_argument('--knn', dest='no_knn', type=int, default=16, help='Number of nearest neighbors in dgcnn layers')
         self.parser.add_argument('--transforminputfeatures', dest='transform_in_features', action='store_true', default='False')
