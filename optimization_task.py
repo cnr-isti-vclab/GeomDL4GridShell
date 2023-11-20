fe9511e441a9cff20ad0d7f3e1747c5a9ec747f2
Andrea Favilli <62485881+poseidon92@users.noreply.github.com>
2023-11-20 15:08:59 +0100

    Changed default options for argparse

--- optimization_task.py
+++ optimization_task.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     parser = NetOptimizerOptions()
     options = parser.parse()
     lo = StructuralNetOptimizer(options.path, options.lr, options.momentum, options.device, options.loss_type, options.no_knn, options.transform_in_features, options.get_loss, options.layer_mode)
-    lo.optimize(options.n_iter, options.save, options.save_interval, options.display_interval, options.save_label, options.take_times, options.neighbor_list, options.save_prefix)
+    lo.optimize(options.max_iter, options.save, options.save_interval, options.display_interval, options.save_label, options.take_times, options.neighbor_list, options.save_prefix)
